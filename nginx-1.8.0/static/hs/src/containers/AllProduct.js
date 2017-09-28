4fb38ae5c1579dd8b987e8f9309e4ead24ddef05
heyaopeng <he_yaopeng@163.com>
2017-09-28 12:26:28 +0800

    fenlei

--- nginx-1.8.0/static/hs/src/containers/AllProduct.js
+++ nginx-1.8.0/static/hs/src/containers/AllProduct.js
@@ -15,9 +15,9 @@ class AllProductsCats extends React.Component{
 		var cats = this.props.cats.map(function(item, index) {
 			return (
 				<div className="all-prod-cat" onClick={()=>{window.location='/all-product?categoryId=' + item.categoryId}}>
-					<span className="all-prod-cat-icon">
+					{/*<span className="all-prod-cat-icon">
 						{item.category.slice(0, 1)}
-					</span>
+					</span>*/}
 					{item.category}
 				</div>
 				);
