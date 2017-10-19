fa9c2a1c9dc019f3deb40a5b013f733b5036b62b
heyaopeng <he_yaopeng@163.com>
2017-10-19 10:36:55 +0800

    .

--- nginx-1.8.0/static/hs-admin/src/containers/CommentList.js
+++ nginx-1.8.0/static/hs-admin/src/containers/CommentList.js
@@ -21,9 +21,7 @@ class CommentList extends React.Component {
       isOpen:false,
       comment:{},
       commentId:0,
-      backTitle:'',
-      commentTmp:'',
-      showDetail:false
+      backTitle:''
     }
   }
 
@@ -43,18 +41,6 @@ class CommentList extends React.Component {
       isOpen: true,
       commentId:id,
       backTitle:text
-    });
-  }
-  openDetail = (item) => {
-    this.setState({
-      showDetail:true,
-      commentTmp:item
-    });
-  }
-   _closeDetail = () => {
-    this.setState({
-      showDetail:false,
-      commentTmp:{}
     });
   }
    
@@ -87,7 +73,7 @@ class CommentList extends React.Component {
     var list = this.state.comments.map((item, index) => {
       return (
         <tr key={index}>
-          {/*<td width="20%" style={{
+          <td width="20%" style={{
             textAlign:'center',verticalAlign:'middle'
           }}>
             <div className='comment-list-order'>
@@ -95,20 +81,15 @@ class CommentList extends React.Component {
               item.order&&item.order.items?item.order.items.map((item1,index1)=>{
                 return(
                   <div className='comment-list-order-item' key={index1}>
-                    <img src={item1.imageUrl} className='image' width='30px' height='30px'/>
+                    
                     <span className='title'> {item1.title} </span>
                   </div>
                 )
               }):null
             }
             </div>
-          </td>*/}
-          <td width="20%" style={{
-            textAlign:'center',verticalAlign:'middle'
-          }}>{item.order.orderSerialNum}</td>
-          {/*<td width="15%" style={{
-            textAlign:'center',verticalAlign:'middle'
-          }}>{item.title}</td>*/}
+          </td>
+          
           <td width="10%" style={{
             textAlign:'center',verticalAlign:'middle'
           }}>{item.deliver}</td>
@@ -118,12 +99,15 @@ class CommentList extends React.Component {
           <td width="10%" style={{
             textAlign:'center',verticalAlign:'middle'
           }}>{item.quality}</td>
+          <td width="15%" style={{
+            textAlign:'center',verticalAlign:'middle',fontSize:'12px'
+          }}>{item.title}</td>
           <td width="7%" style={{
             textAlign:'center',verticalAlign:'middle'
           }}>{item.timeStr}</td>
-          {/*<td width="15%"    style={{
-            textAlign:'center',verticalAlign:'middle'
-          }}>{item.backTitle}</td>*/}
+          <td width="15%"    style={{
+            textAlign:'center',verticalAlign:'middle',fontSize:'12px'
+          }}>{item.backTitle}</td>
           <td width="7%" style={{
             textAlign:'center',verticalAlign:'middle'
           }}>{item.backTimeStr}</td>
@@ -133,29 +117,26 @@ class CommentList extends React.Component {
             <button className="btn btn-primary" onClick={this.openModal.bind(this,item.id,item.backTitle)}>
               回复
             </button>
-            <button className="btn btn-primary" style={{marginTop:'5px'}}  onClick={this.openDetail.bind(this,item)}>
-              查看详情
-            </button>
-            {/*<button className="btn btn-primary" style={{marginTop:'5px'}} onClick={this.delHandle.bind(this,item.id)}>
+            <button className="btn btn-primary" style={{marginTop:'5px'}} onClick={this.delHandle.bind(this,item.id)}>
               删除回复
-            </button>*/}
+            </button>
           </td>
         </tr>
       )
     })
     return (
-      <DocumentTitle title="评论管理">
+      <DocumentTitle title="评论列表">
         <div className="comment-list">
             <table className="table table-bordered table-striped" width='135%'>
               <thead width='135%'>
                 <tr>
-                  <th width="25%">订单号</th>
-                  {/*<th width="20%">评论内容</th>*/}
-                  <th width="15%">送货速度评分</th>
-                  <th width="15%">服务态度评分</th>
-                  <th width="15%">产品质量评分</th>
+                  <th width="20%">订单信息</th>
+                  <th width="10%">送货速度评分</th>
+                  <th width="10%">服务态度评分</th>
+                  <th width="10%">产品质量评分</th>
+                  <th width="20%">评论内容</th>
                   <th width="15%">评论时间</th>
-                  {/*<th width="20%">后台回复</th>*/}
+                  <th width="20%">后台回复</th>
                   <th width="15%">回复时间</th>
                   <th width="15%">
                     <a href=""></a>操作</th>
@@ -165,41 +146,6 @@ class CommentList extends React.Component {
                 {list}
               </tbody>
             </table>
-            {
-              this.state.showDetail?
-              <div className='comment-detail'>
-                <i className='fa fa-times-circle-o icon' onClick={this._closeDetail}/>
-                <div className='comment-detail-order'>
-                {
-                  this.state.commentTmp.order&&this.state.commentTmp.order.items?this.state.commentTmp.order.items.map((item1,index1)=>{
-                    return(
-                      <div className='comment-detail-order-item' key={index1}>
-                        <img src={item1.imageUrl} className='image' width='30px' height='30px'/>
-                        <span className='title'> {item1.title} </span>
-                      </div>
-                    )
-                  }):null
-                }
-                </div>
-                <div className="form-group">
-                  <label className="control-label col-md-3">评论内容：</label>
-                  <textarea  className="comment-textarea" disabled rows='10' style={{width:'90%',marginLeft:'2%' }}  value={this.state.commentTmp.title}>
-
-                    </textarea>
-                </div>
-                <div className="form-group">
-                  <label className="control-label col-md-3">系统回复：</label>
-                  <textarea  className="comment-textarea" disabled rows='10' style={{width:'90%',marginLeft:'2%' }}  value={this.state.commentTmp.backTitle}>
-
-                    </textarea>
-                </div>
-                <div className="form-group">
-                  <label className="control-label col-md-3">系统回复时间：</label>
-                  <input type='text' disabled value={this.state.commentTmp.backTimeStr}/>
-                </div>
-            </div>:null
-            }
-            
             <Modal isOpen={this.state.isOpen} onRequestHide={this.hideModal}>
               <ModalHeader>
                 <ModalClose onClick={this.hideModal}/>
