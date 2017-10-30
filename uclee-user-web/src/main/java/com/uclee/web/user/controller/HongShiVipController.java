7be1b139950ba1be5f855da9ad7c4eb98529558b
heyaopeng <he_yaopeng@163.com>
2017-10-30 11:51:55 +0800

    修改验证码

--- uclee-user-web/src/main/java/com/uclee/web/user/controller/HongShiVipController.java
+++ uclee-user-web/src/main/java/com/uclee/web/user/controller/HongShiVipController.java
@@ -140,8 +140,8 @@ public class HongShiVipController {
 			ret.put("reason", "没传数据");
 			return ret;
 		}
-		if(vip.getcMobileNumber()==null||vip.getcMobileNumber().isEmpty()){
-			if(vip.getCode()==null||vip.getCode().isEmpty()){
+		if(vip.getcMobileNumber()==null||!vip.getcMobileNumber().isEmpty()){
+			if(vip.getCode()==null||!vip.getCode().isEmpty()){
 				if(!VerifyCode.checkVerifyCode(session,vip.getcMobileNumber(),vip.getCode())){
 					ret.put("reason", "验证码错误");
 					return ret;
