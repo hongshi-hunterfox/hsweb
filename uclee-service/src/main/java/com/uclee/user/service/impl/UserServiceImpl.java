2710d4c21eeda70e1fffc69bb35c49bdb449ede6
heyaopeng <he_yaopeng@163.com>
2017-11-15 09:32:35 +0800

    .

--- uclee-service/src/main/java/com/uclee/user/service/impl/UserServiceImpl.java
+++ uclee-service/src/main/java/com/uclee/user/service/impl/UserServiceImpl.java
@@ -2758,6 +2758,7 @@ public class UserServiceImpl implements UserServiceI {
 				order.setDiscount(discount);
 				order.setTotalAmount(total.doubleValue());
 				BigDecimal account = total.add(order.getShippingCost()).subtract(discount);
+				account = account.subtract(order.getCut());
 				order.setAccounts(account.doubleValue());
 			}
 			List<HongShiOrder> ordersRet = new ArrayList<HongShiOrder>();
